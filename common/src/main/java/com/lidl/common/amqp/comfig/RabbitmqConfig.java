8cfceca629a1cc3977ba0234aa0e6f9617f4c456
lidl <3691745@qq.com>
2017-09-25 11:31:56 +0800

    update

--- common/src/main/java/com/lidl/common/amqp/comfig/RabbitmqConfig.java
+++ common/src/main/java/com/lidl/common/amqp/comfig/RabbitmqConfig.java
@@ -1,9 +1,13 @@
 package com.lidl.common.amqp.comfig;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.Receiver;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -45,7 +49,7 @@ public class RabbitmqConfig {
         return BindingBuilder.bind(queue).to(directExchange).with(ROUTINGKEY_NAME);
     }
 
-    /*
+
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
                                              MessageListenerAdapter listenerAdapter) {
@@ -60,5 +64,5 @@ public class RabbitmqConfig {
     MessageListenerAdapter listenerAdapter(Receiver receiver) {
         return new MessageListenerAdapter(receiver, "receiveMessage");
     }
-    */
+
 }
--- consumer/src/main/java/com/lidl/consumer/amqp/message/Receiver.java
+++ consumer/src/main/java/com/lidl/consumer/amqp/message/Receiver.java
@@ -1,22 +1,22 @@
 package com.lidl.consumer.amqp.message;
 
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.logging.Logger;
 
 @Component
 public class Receiver {
 
-    Logger logger = Logger.getLogger(Receiver.class.toString());
+    private static final Logger logger = Logger.getLogger(Receiver.class.toString());
 
-    //接收到消息处理.
-    @RabbitListener(queues = "test_consume")
+    // -> no return callback.
+//    @RabbitListener(queues = "test_consume")
     public void onMessage(@Payload String foo) throws InterruptedException {
+//        throw new RuntimeException();
         logger.info(">>> 消费信息 " + foo + " 成功！");
-
 //        StopWatch watch = new StopWatch();
 //        watch.start();
 //        System.out.println("instance " + 1 + " [x] Received '" + foo + "'");
@@ -25,12 +25,20 @@ public class Receiver {
 //        System.out.println("instance " + 1 + " [x] Done in " + watch.getTotalTimeSeconds() + "s");
     }
 
-//    private void doWork(String in) throws InterruptedException {
-//        for (char ch : in.toCharArray()) {
-//            if (ch == '.') {
-//                Thread.sleep(1000);
-//            }
-//        }
-//    }
+    // -> return callback
+    @RabbitListener(queues = "test_consume")
+    public String onMessageByReturn(String s) throws InterruptedException {
+//        Thread.sleep(10000);
+        logger.info(">>> 消费信息 " + s + " 成功！");
+        return "successful";
+    }
 
+
+    private void doWork(String in) throws InterruptedException {
+        for (char ch : in.toCharArray()) {
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
 }
