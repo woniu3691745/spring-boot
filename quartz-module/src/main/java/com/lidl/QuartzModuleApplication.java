c2ecd78fe766dfacd75a3f609e948bc7ba3e965d
lidl <3691745@qq.com>
2018-01-16 18:02:42 +0800

    quartz

---
+++ quartz-module/src/main/java/com/lidl/QuartzModuleApplication.java
@@ -0,0 +1,12 @@
+package com.lidl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzModuleApplication.class, args);
+	}
+}
